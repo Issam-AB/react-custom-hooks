4348180fe68bb4d5367c0302bfaf747a9af2bda0
Issam <Issam.aboulfadl05@gmail.com>
2022-05-22 05:26:35 +0100

    feat: add useTimeout

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import ToggleComponent from "./1-useToggle/ToggleComponent";
+import TimeoutComponent from "./2-useTimeout/TimeoutComponent";
 
 function App() {
 	return (
 		<div className="App">
-			<ToggleComponent />
+			{/* <ToggleComponent /> */}
+			<TimeoutComponent />
 		</div>
 	);
 }
